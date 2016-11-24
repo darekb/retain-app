4744baf7559f01f9e27a998c8b58c46cee0f8c4a
darekb <kontakt@3wm.pl>
2016-11-24 22:58:20 +0100

    end lesson

---
+++ src/app/containers/auth.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by darek on 24.11.16.
+ */
+import { Component } from '@angular/core';
+@Component({
+    selector: 'auth-container',
+    template: `
+    <div class="auth-container">
+      <h1>Auth</h1>
+    </div>
+  `
+})
+export class Auth {}
---
+++ src/app/services/auth.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import '../../../node_modules/rxjs/Rx.d';
+@Injectable()
+export class AuthService implements CanActivate {
+    JWT_KEY:string = 'retain_token';
+    JWT:string = '';
+
+    constructor(private router:Router) {}
+
+    isAuthorized():boolean {
+        return Boolean(this.JWT);
+    }
+
+    canActivate():boolean {
+        const canActivate = this.isAuthorized();
+        this.onCanActivate(canActivate);
+        return canActivate;
+    }
+
+    onCanActivate(canActivate:boolean) {
+        if (!canActivate) {
+            this.router.navigate(['', 'auth']);
+        }
+    }
+}
