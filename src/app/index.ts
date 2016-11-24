97518387f08ea824ec1afe8b8c262eae2b4b48b9
darekb <kontakt@3wm.pl>
2016-11-24 13:11:51 +0100

    end lesson

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,1 +1,25 @@
+import * as services from './services';
 export { App } from './app';
+
+const mapValuesToArray = (obj) => Object.keys(obj).map(key => obj[key]);
+
+export const providers = [
+	...mapValuesToArray(services)
+]
+
+/*
+
+var mapValuesToArray = function (obj) { 
+	return Object.keys(obj).map(function (key) { 
+		return obj[key]; 
+	}); 
+};
+
+var test = {
+	'one':1,
+	'two': 2,
+	'three': 3
+}
+mapValuesToArray(test);
+
+*/
